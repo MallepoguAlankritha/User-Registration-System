c1c847c4c972ddc98de6d0110931eef90bbfed89
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-22 17:56:43 +0530

    Added:Enter valid Lastname

--- com/bridgelabz/regex/UserRegistrationMain.java
+++ com/bridgelabz/regex/UserRegistrationMain.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class UserRegistrationMain {
 
     public static void welcome(){
-        System.out.println("Welcome to User Registration System Problem");
+        System.out.println("Welcome to User Registration System ");
     }
     public static void main(String[] args) {
 	    welcome();
         UserRegistration user = new UserRegistration();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 : to validate First Name ");
+        System.out.println("Enter 2 : to validate Last Name ");
         switch (scanner.nextInt()){
             case 1:
                 UserRegistration.validFirstName();
                 break;
+            case 2:
+                UserRegistration.validLastName();
+                break;
+            default:
+                System.out.println("Select a valid number");
         }
     }
 }
+
